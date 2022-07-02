927919a238c2b839ec3812823a85db848287db37
Gurvir Sandhar <gurvirsandhar@gmail.com>
2022-07-01 18:02:34 -0700

    created Record model

---
+++ Models/Record.cs
@@ -0,0 +1,16 @@
+﻿namespace VehicleMaintenanceTracker.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public int Mileage { get; set; }
+        public string WorkDone { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public Record()
+        {
+
+        }
+
+    }
+}
