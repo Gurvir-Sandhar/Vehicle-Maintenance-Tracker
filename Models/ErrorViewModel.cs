6e846d3854fa0df5e4ffed7ba64e6946b6aabad4
Gurvir Sandhar <gurvirsandhar@gmail.com>
2022-07-01 17:41:05 -0700

    created asp.net template project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleMaintenanceTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
